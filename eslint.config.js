631cf381ada975aa876cf262faa417be57f31900
AppVeyor <yask.alexandr@gmail.com>
2024-10-13 18:45:25 +0300

    change rules10

--- eslint.config.js
+++ eslint.config.js
@@ -23,9 +23,6 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    extends: {
-      'plugin':react/recommended,
-    },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
